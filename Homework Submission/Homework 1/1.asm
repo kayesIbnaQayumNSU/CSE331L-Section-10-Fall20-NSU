1a42a2575dc6ee78c36ef5653a188b7a3cf91a3e
KayesQayum <kayes.qayum@northsouth.edu>
2020-11-17 23:04:42 +0600

    homework 1 added

---
+++ Homework Submission/Homework 1/1.asm
@@ -0,0 +1,17 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+MOV AX, 2
+ADD AX, 1
+ADD AX, 1
+
+MOV CX, AX
+
+ret
+
+
+
+
